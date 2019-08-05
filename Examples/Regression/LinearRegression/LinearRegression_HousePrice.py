7edee705005c45358b38f072c4781b87fec5a751
sfonly <1652229453@qq.com>
2019-08-05 18:26:57 +0800

    Update LinearRegression_HousePrice.py

--- Examples/Regression/LinearRegression/LinearRegression_HousePrice.py
+++ Examples/Regression/LinearRegression/LinearRegression_HousePrice.py
@@ -1,105 +1,113 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Thu Jul  2 09:43:26 2019
 
-This is a temporary script file.
+LinearRegression_HousePrice
+
+@author: sf_on
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np 
-data = pd.read_csv(open('./HousePrice.csv'))
+import seaborn as sns
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
+from sklearn.preprocessing import MinMaxScaler
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+# 查看列和类标号之间的关联关系
+def show_col2label(data,col):
+    plt.subplots(figsize=(8,8))
+    plt.scatter(data[col],y,s = 20)
+    plt.title(col)  
+    plt.show()
+
+# 绘制查看预测曲线和真实值的偏差
+def show_line(y_test, y_predict):
+    predata = pd.DataFrame(columns=('MEDV','P_MEDV'))
+    predata['MEDV'] = y_test['MEDV']
+    predata['P_MEDV'] = y_predict
+    predata = predata.sort_index(by='P_MEDV',axis=0,ascending=True)
+    plt.subplots(figsize=(14,7))
+    plt.plot(range(len(predata)),predata['MEDV'],'green',linewidth=2.5,label="real_data")
+    plt.plot(range(len(predata)),predata['P_MEDV'],'red',linewidth=2.5,label="pre_data")
+    plt.legend(loc=2)
+    plt.show()
+
+# 查找y相关性最高的k个变量
+def find_k_important_feature(x,y,k=5):
+    Kbest = SelectKBest(f_regression, k)
+    Kbest.fit_transform(x,y)
+    return x.columns[Kbest.get_support()]
+
+# 绘制相关性矩阵
+def show_minmaxdata_matrix(data):
+    pd.plotting.scatter_matrix(data, alpha=0.7, figsize=(16,16), diagonal='hist')
+    plt.rcParams['font.sans-serif']=['SimHei']
+    plt.show()
+
+# 同时drop特征与类标号
+def dropdata(x,y,i_): 
+    x.drop(i_,axis=0,inplace=True)
+    y.drop(i_,axis=0,inplace=True)
+
+def show_corr(data):
+    plt.figure(figsize=(10,8))
+    g = sns.heatmap(data.corr(),annot=True,fmt = '.2f',cmap = 'coolwarm')
+    g.set_xlabel('corr')
+    plt.show()
+
 # 读入数据
+data = pd.read_csv(open('./HousePrice.csv'))
 
-#data.isnull().any().sum()
+# 数据探索
+data.isnull().any().sum()
 
-#pd.plotting.scatter_matrix(data, alpha=0.5, figsize=(10,10), diagonal='kde')
-
-corr = data.corr()
-
-x = data[['CRIM','CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD',
+# 设置变量和类标号
+x = data[['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD',
           'TAX','PTRATIO','B','LSTAT']]
 y = data[['MEDV']]
-# 设置变量和类标号
 
-# 找到和y相关性最高的3个变量
-from sklearn.feature_selection import SelectKBest 
-from sklearn.feature_selection import f_regression
-SelectKBest = SelectKBest(f_regression, k=3)
-bestFeature = SelectKBest.fit_transform(x,y)
-SelectKBest.get_support()
-x.columns[SelectKBest.get_support()]
+# 查看特征的相关性
+show_corr(x)
+# drop 掉 RAD
+x.drop('RAD', axis=1,inplace=True)
 
-features = data[x.columns[SelectKBest.get_support()]]
-pd.plotting.scatter_matrix(features, c='b',alpha=0.7, figsize=(12,12), diagonal='hist')
-plt.subplots(figsize=(8,8))
-plt.scatter(features['LSTAT'],y,s = 20)
-plt.title('LSTAT')  
-plt.show()
+# 寻找和类标号最相关的 k 个特征
+index = find_k_important_feature(x,y, 12)
+features = data[index]
+
+# 画出每个特征和类标号的关联性
+for col in index:   
+    show_col2label(features,col)
 
 # 获取y中MEDV等于50的index，并存储在list中
 i_ = y[y.MEDV == 50].index.tolist()
-print(i_)
-y.drop(i_,axis=0,inplace=True) #删除异常值
-features.drop(i_,axis=0,inplace=True) #删除异常值
+dropdata(features,y,i_)
 
-from sklearn.preprocessing import MinMaxScaler
+# 数据归一化
 scaler = MinMaxScaler()
-for feature in features.columns:
-    features['标准化'+feature] = scaler.fit_transform(features[[feature]])
-pd.plotting.scatter_matrix(
-        features[['标准化RM','标准化PTRATIO', '标准化LSTAT']], 
-        alpha=0.7, figsize=(8,8), diagonal='hist')
+columns = features.columns.tolist()
+features_MinMaxScaler = pd.DataFrame(scaler.fit_transform(features), columns=columns)
 
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(
-        features[['标准化RM','标准化PTRATIO', '标准化LSTAT']],y, test_size=0.3,random_state=33)
+# 数据归一化后展示
+show_minmaxdata_matrix(features_MinMaxScaler)
 
-# =============================================================================
-# 生成训练集csv文件
-# Traindata = pd.DataFrame(columns=('INDUS','RM', 'TAX','PTRATIO','LSTAT','MEDV'))
-# Traindata['INDUS'] = x_train['标准化INDUS']
-# Traindata['RM'] = x_train['标准化RM']
-# Traindata['TAX'] = x_train['标准化TAX']
-# Traindata['PTRATIO'] = x_train['标准化PTRATIO']
-# Traindata['LSTAT'] = x_train['标准化LSTAT']
-# Traindata['MEDV'] = y_train['MEDV']
-# Traindata.to_csv('C://Users/sf_on/Desktop/houseprice_train.csv',index = False,encoding = 'utf-8')
-# =============================================================================
-
-# =============================================================================
-# Testdata = pd.DataFrame(columns=('INDUS','RM', 'TAX','PTRATIO','LSTAT','MEDV'))
-# 生成测试集csv文件
-# Testdata['INDUS'] = x_test['标准化INDUS']
-# Testdata['RM'] = x_test['标准化RM']
-# Testdata['TAX'] = x_test['标准化TAX']
-# Testdata['PTRATIO'] = x_test['标准化PTRATIO']
-# Testdata['LSTAT'] = x_test['标准化LSTAT']
-# Testdata['MEDV'] = y_test['MEDV']
-# Testdata.to_csv('C://Users/sf_on/Desktop/houseprice_test.csv',index = False,encoding = 'utf-8')
-# =============================================================================
+# 分割数据集
+x_train, x_test, y_train, y_test = train_test_split(features_MinMaxScaler,y, test_size=0.3,random_state=32)
     
-from sklearn import linear_model
 lr = linear_model.LinearRegression()
 lr.fit(x_train,y_train)
-print(lr.coef_)
-print(lr.intercept_)
-
 y_predict=lr.predict(x_test)
-lr.score(x_train,y_train)
-lr.score(x_test,y_test)
 
-ROCdata = pd.DataFrame(columns=('MEDV','P_MEDV'))
-ROCdata['MEDV'] = y_test['MEDV']
-ROCdata['P_MEDV'] = y_predict
+print('lr_model coef:', lr.coef_)
+print('lr_model intercept:', lr.intercept_)
+print('lr_model train_score:', lr.score(x_train,y_train))
+print('lr_model test_score:', lr.score(x_test,y_test))
+print("Mean squared error: %.2f" % mean_squared_error(y_test, y_predict))    
 
-from sklearn.metrics import mean_squared_error
-print("Mean squared error: %.2f" % mean_squared_error(y_test, y_predict))
+show_line(y_test, y_predict)
 
-ROCdata = ROCdata.sort_index(by='P_MEDV',axis=0,ascending=True)
-plt.subplots(figsize=(14,7))
-plt.plot(range(len(ROCdata)),ROCdata['MEDV'],'green',linewidth=2.5,label="real_data")
-plt.plot(range(len(ROCdata)),ROCdata['P_MEDV'],'red',linewidth=2.5,label="pre_data")
-plt.legend(loc=2)
-plt.show()
 
